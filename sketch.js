1e3b0479981b0757e6d7f715244d8f404ba8a35e
Swastik7 <97216741+Swastik7@users.noreply.github.com>
2022-03-12 12:45:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var s1,s2;
+function setup() {
+createCanvas(400,400);
+s1=new Student("jon",11,5);
+s1.display();
+
+s2=new Student("josh",12,6);
+s2.display();
+
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,25 @@
+
+class Student
+{
+constructor(name,age,grade) 
+{ 
+this.name=name;
+this.age=age;
+this.grade=grade
+}
+display()
+{
+console.log(this.name)
+console.log(this.grade)
+console.log(this.age)
+}
+
+
+
+
+
+
+}
+
+
+
